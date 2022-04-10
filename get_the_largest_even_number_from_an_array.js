b1df38ec5a102094652f11666b933088aed94fab
MavFeuille <mavis.feuille@gmail.com>
2022-04-10 16:08:11 -0400

    Input instructions

---
+++ get_the_largest_even_number_from_an_array.js
@@ -0,0 +1,8 @@
+/* Given an integer array, get the largest even number from the array, return -1 if not found.
+
+    Sample Code:
+
+    function getTheLargest(arr) {
+    // Enter your logic here...
+    }
+*/
