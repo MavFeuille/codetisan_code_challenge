8629c272365cad988e88b1d4c04f5f1df625344c
MavFeuille <mavis.feuille@gmail.com>
2022-04-10 15:53:34 -0400

    Input instructions

---
+++ convert_a_number_to_hours_and_minutes.js
@@ -0,0 +1,7 @@
+/* Write a JavaScript function to convert a given number in minutes to hours and minutes.
+    Examples:
+
+    Input: 235
+    Output: '3:55'
+    Explanation: 235 is equal to 3 hours and 55 minutes.
+*/
