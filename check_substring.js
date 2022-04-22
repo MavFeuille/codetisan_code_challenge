d64be5fcaebd51f446f92d043f36f86ce9d4677d
MavFeuille <mavis.feuille@gmail.com>
2022-04-22 17:49:29 -0400

    Set up frame for function checkString

--- check_substring.js
+++ check_substring.js
@@ -1,3 +1,7 @@
 /* Write a JavaScript function to check whether a string contains 'code' string, 
    return true if it contains 'code' string or return false otherwise.
 */
+
+const checkString = (str, search) => {
+
+};
