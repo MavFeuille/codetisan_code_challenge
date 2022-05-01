778539d13ff77a1b9f80e79f7b25965a0dd1c17d
MavFeuille <mavis.feuille@gmail.com>
2022-04-30 23:59:45 -0400

    Remove comments

--- check_substring.js
+++ check_substring.js
@@ -4,12 +4,10 @@
 
 const checkString = (str, search) => {
   let strLowerCase = str.toLowerCase();
-//   console.log("🚀 ~ file: check_substring.js ~ line 7 ~ checkString ~ newStr", newStr);
   let searchLowerCase = search.toLowerCase();
-//   console.log("🚀 ~ file: check_substring.js ~ line 9 ~ checkString ~ searchLowerCase", searchLowerCase)
   return strLowerCase.includes(searchLowerCase) ? true : false;
 };
 
-// console.log(checkString('Codetisan', 'code'));
-// console.log(checkString('source code', 'CODE'));
+console.log(checkString('Codetisan', 'code'));
+console.log(checkString('source code', 'CODE'));
 console.log(checkString('Developer', 'code'));
